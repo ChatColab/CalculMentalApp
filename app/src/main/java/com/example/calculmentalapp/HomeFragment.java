3ebd9e9866b18f317e47d21998dea1b5ae574b84
Florian <escande.florian.chatco@gmail.com>
2023-05-03 19:20:55 +0200

    introduce-gamefragment

--- app/src/main/java/com/example/calculmentalapp/HomeFragment.java
+++ app/src/main/java/com/example/calculmentalapp/HomeFragment.java
@@ -9,7 +9,6 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.calculmentalapp.databinding.FragmentFirstBinding;
 import com.example.calculmentalapp.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
